d29bef4f6c1eba3538e25cb9f56bef39f4cb0016
eMobilis <samuelnjihia81@gmail.com>
2023-06-27 10:16:20 +0300

    betting website

---
+++ betting.py
@@ -0,0 +1,27 @@
+#create a simple reward system
+#use any number 1-5
+#they are rewarded
+
+bettingnumber =  int(input("Enter any number between 1-5")
+if  bettingnumber == 1:
+    print(" You have won a herd of cows")
+elif bettingnumber == 2:
+    print(" You have won a Mercedes car")
+elif bettingnumber == 3:
+    print("You have won a mansion")
+elif bettingnumber == 4:
+    print("You have won a house")
+elif bettingnumber == 5:
+    print("You have won 100,000 dollars")
+else:
+    print("Invalid Number entered")
+
+
+
+
+
+
+
+
+
+
