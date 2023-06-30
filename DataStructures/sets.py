6795e0860520121481eb9572006500eec063b907
eMobilis <samuelnjihia81@gmail.com>
2023-06-30 08:57:27 +0300

    sets

---
+++ DataStructures/sets.py
@@ -0,0 +1,13 @@
+#Defined by curly brackets
+#order is not guaranteed
+#Set does not allow duplicates
+
+
+myfriends = {"Joe", "Mike", "Louis" ,"Tracy"}
+myfriends.add("Joe")
+print(myfriends)
+
+
+
+
+
