519e6868f1ffc00c4293f232e8dc84482b2f2afc
eMobilis <samuelnjihia81@gmail.com>
2023-07-04 09:00:49 +0300

    updated pycharm lessons

---
+++ Modules/module1.py
@@ -0,0 +1,18 @@
+def  addtwonumbers(number1, number2):
+    return  number1 + number2
+
+def  subtwonumbers(number1, number2):
+    return  number1 - number2
+
+def  multtwonumbers(number1, number2):
+    return  number1 * number2
+
+
+
+
+
+
+
+
+
+
---
+++ Modules/module2.py
@@ -0,0 +1,4 @@
+from module1 import *
+
+print(addtwonumbers(65,30))
+print(multtwonumbers(34,43))
