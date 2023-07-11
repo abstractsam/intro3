6a377b0559e7e084c262373bfe9874dd04c395f6
eMobilis <samuelnjihia81@gmail.com>
2023-07-11 09:36:15 +0300

    Encapsulation

--- OOP/Encapsulation.py
+++ OOP/Encapsulation.py
@@ -8,18 +8,19 @@ class Base:
 
 class Derived(Base):
     def __init__(self):
-        print(self.a)
-        print(self.b)
-        print(self.c)
-        print(self.d)
+        print(self.a)  # inaccessible
+        print(self.b)  # accessible
+        print(self.c)  # accessible
+        print(self.d)  # accessible
 
-#Create an instance or duplicate of parent class
-#you can hide proprties i code using (_) or underscore
+
+# Create an instance or duplicate of parent class
+# you can hide proprties i code using (_) or underscore
 
 obj1 = Base()
-#print(obj1.a)
+# print(obj1.a) - "You don't have rights and permission"
 print(obj1.b)
 print(obj1.c)
 print(obj1.d)
 
-
+# https://justpaste.it/adl6w
